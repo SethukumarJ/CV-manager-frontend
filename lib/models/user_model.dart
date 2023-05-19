01bd936e7c209a2b962bf51ddf6c2fd44a751dc4
Sethukumar J <sethukumarj.76@gmail.com>
2023-05-19 11:38:52 +0530

    created user model

---
+++ lib/models/user_model.dart
@@ -0,0 +1,4 @@
+class User{
+  final String cv,dob,phone,firstName,lastName,email;
+  User({required this.cv,required this.dob,required this.phone,required this.firstName,required this.lastName,required this.email});
+}
